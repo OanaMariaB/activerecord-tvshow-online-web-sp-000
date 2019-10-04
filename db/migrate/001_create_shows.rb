2900c49a4bc7bf780be565d02f2e20a29680846c
Oana Berentan <oana.berentan@yahoo.com>
2019-10-04 08:54:13 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,2 +1,9 @@
 class CreateShows < ActiveRecord::Migrations[5.2]
+  def change 
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :ratings
+  end
 end
